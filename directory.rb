53eb62b77c29cf521753d3b1719c6e9553c6e553
Emily Sesto <emilyalicesesto@gmail.com>
2020-09-22 17:42:44 +0100

    Completed step 13 - takes arguments from command line

--- directory.rb
+++ directory.rb
@@ -3,14 +3,14 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # Get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # While the name is not empty, repeat this code
   while !name.empty? do
     # Add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # Get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   # Return the array of students
 end
@@ -56,13 +56,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
 end
 
 def process(selection)
@@ -85,8 +97,9 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
+try_load_students
 interactive_menu
